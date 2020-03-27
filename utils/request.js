0b429964fec21db662fde19ec30d7d3fc678ee9d
gpw <653015681@qq.com>
2020-03-27 10:15:31 +0800

    "2020/03/27"

--- utils/request.js
+++ utils/request.js
@@ -16,27 +16,38 @@ function getJSON() {
   })
 }
 
-// function fetch (parmas) {
-//   let header = {}
-//   if(parmas.needToken) token = wx.getStorageSync('token')
-//   return new Promise(function (resolve,reject){
-//     wx.request({
-//       url:parmas.url,
-//       data:parmas.data,
-//       header:{
-//         ...header,
-//         ...parmas.header
-//       },
-//       method: parmas.method || 'GET',
-//       success:()=>{
-//         resolve(res)
-//       },
-//       fail:()=>{
-//         // if () {
-  
-//         // }
-//         reject(err)
-//       }
-//     })
-//   })
-// }
+function fetch (parmas) {
+  let header = {}
+  if(parmas.needToken) token = wx.getStorageSync('token')
+  let promise = new Promise(function (resolve,reject){
+    wx.request({
+      url:parmas.url,
+      data:parmas.data,
+      header:{
+        ...header,
+        ...parmas.header
+      },
+      method: parmas.method || 'GET',
+      success:()=>{
+        resolve(res)
+      },
+      fail:()=>{
+        // if () {
+
+        // }
+        reject(err)
+      }
+    })
+  });
+
+  promise.then(s => {
+    console.log(s);
+    return s;
+  }, e => {
+    console.log(e);
+  })
+}
+
+module.exports = {
+  fetch
+}
