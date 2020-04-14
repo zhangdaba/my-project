97b31253528de3aebf1c2b3f21d34e9e58f9e9b6
gpw <653015681@qq.com>
2020-04-14 13:16:32 +0800

    [add]:getUpdateManager

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,7 +17,34 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
+    this.versionUpdating()
+  },
 
+  versionUpdating() {
+    const updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+      if(res.hasUpdate) {
+        updateManager.onUpdateReady(function () {
+          wx.showModal({
+            title: '更新提示',
+            content: '新版本已经准备好，是否重启应用？',
+            success: function (res) {
+              if (res.confirm) {
+                // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+                updateManager.applyUpdate()
+              }
+            }
+          })
+        })
+      }
+    })
+
+    updateManager.onUpdateFailed(function () {
+      // 新版本下载失败
+      console.log('新版本下载失败')
+    })
   },
 
   handleLogin: function (e) {
@@ -62,7 +89,7 @@ Page({
         }
       },
       fail(err) {
-        if(err.errMsg == 'request:fail timeout' || 'request:fail') {
+        if (err.errMsg == 'request:fail timeout' || 'request:fail') {
           util.showTextToast('连接超时，请稍后再试...');
           _this.setData({
             loadinGdis: false,
